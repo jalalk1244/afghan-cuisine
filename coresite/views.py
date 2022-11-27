c781bf1bdf7c980a70d2a3746dbdc3b424384041
jalalk1244 <jalaluddin.boss@gmail.com>
2022-11-27 10:31:35 +0000

    Create view for contact page and add a contact form

--- coresite/views.py
+++ coresite/views.py
@@ -6,3 +6,8 @@ from django.shortcuts import render
 def index(request):
     # return the 'index.html' template
     return render(request, 'index.html')
+
+
+def contact(request):
+    # Get the submited contact form details
+    return render(request, 'contact.html')
