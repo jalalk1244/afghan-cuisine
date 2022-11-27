640d5b120652a34156effc58c33082b0ffd17cbf
jalalk1244 <jalaluddin.boss@gmail.com>
2022-11-27 19:21:34 +0000

    First try to send an email

--- afghancuisine/settings.py
+++ afghancuisine/settings.py
@@ -144,3 +144,12 @@ DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+# Email settings
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_USE_TLS = True
+EMAIL_PORT = 587
+EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')
--- coresite/views.py
+++ coresite/views.py
@@ -1,16 +1,26 @@
 from django.shortcuts import render
+from django.core.mail import send_mail
 from .forms import ContactForm
+from afghancuisine import settings
 
 # Create your views here.
 
 
 def index(request):
-    if request.method == 'POST':
-        name = request.POST['fname'] + ' ' + request.POST['lname']
-        email = request.POST['email']
+    '''View for the home page '''
+    contact_form = ContactForm(request.POST or None)
+    if request.method == 'POST' and contact_form.is_valid:
+        # When the contact form is submitted
+        name = f"{request.POST['first_name']} {request.POST['last_name']}"
+        from_email = request.POST['email']
+        subject = f'You have got an email from {name}'
         message = request.POST['message']
+        recipient_list = [settings.EMAIL_HOST_USER]
 
-        return render(request, 'index.html', {'name': name})
+        # Send the details in an emaiil
+        send_mail(subject, message, from_email, recipient_list)
+
+        return render(request, 'index.html', {'name': name, 'contact_form': ContactForm()})
     else:
         # return the 'index.html' template
         return render(request, 'index.html', {'contact_form': ContactForm()})
