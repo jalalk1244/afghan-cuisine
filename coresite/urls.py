b3fd91c723f45ad0e1bb2c04598422fba588a2ab
jalalk1244 <jalaluddin.boss@gmail.com>
2022-11-27 11:00:43 +0000

    Remove the contact view and template and move the contact form to index.html

--- coresite/urls.py
+++ coresite/urls.py
@@ -4,5 +4,4 @@ from django.urls import path
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('contact', views.contact, name='contact')
 ]
--- coresite/views.py
+++ coresite/views.py
@@ -6,8 +6,3 @@ from django.shortcuts import render
 def index(request):
     # return the 'index.html' template
     return render(request, 'index.html')
-
-
-def contact(request):
-    # Get the submited contact form details
-    return render(request, 'contact.html')
