a19cbb39fe68948396fcca76c6758b544a6b7c56
jalalk1244 <jalaluddin.boss@gmail.com>
2022-11-29 08:33:25 +0000

    Create a model for the reservation table

--- reservation/models.py
+++ reservation/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+STATUS = ((0, 'Not approved'), (1, 'Approved'))
+
+
+class Table(models.Model):
+    '''Model for the reservation table '''
+    Name = models.CharField(max_length=20, unique=True)
+    max_num_guest = models.IntegerField()
