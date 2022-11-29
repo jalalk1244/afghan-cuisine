ece0d2e2cc2f0cfdd929dd57fee9aa5553f4e8e8
jalalk1244 <jalaluddin.boss@gmail.com>
2022-11-29 09:21:03 +0000

    Add the view for the reservations page

--- reservation/views.py
+++ reservation/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
+from django.views import generic
+from django.contrib.auth.models import User
+from .models import Reservation
 
-# Create your views here.
+
+
+def get_reservations(self, request, User):
+
+    current_client = request.user
+    reservations = Reservation.objects.filter(client=current_client).order_by('-date_picked')
+
+    return reservations
+
+class ReservationList(generic.ListView):
+    model = Reservation
+    queryset = get_reservations(self, request, User)
+    template_name = 'reservations.html'
