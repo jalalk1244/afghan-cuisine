76a19a7c4c0e249a2b08c29dcfa668ed478105b8
jalalk1244 <jalaluddin.boss@gmail.com>
2022-11-29 14:46:11 +0000

    Add widgets to the reservation form fields

--- reservation/forms.py
+++ reservation/forms.py
@@ -8,3 +8,10 @@ class ReservationForm(ModelForm):
     class Meta:
         model = Reservation
         fields = ('title', 'num_of_guest', 'date_picked', 'time_picked')
+
+        widgets = {
+            'title': forms.TextInput(attrs={'class': 'form-control', 'required': True,}),
+            'num_of_guest': forms.NumberInput(attrs={'class': 'form-control', 'minlength': 1, 'maxlength': 10, 'required': True, 'type': 'number',}),
+            'date_picked': forms.DateInput(attrs={'class': 'form-control', 'type': 'date', 'required': True}),
+            'time_picked': forms.TimeInput(attrs={'class': 'form-control', 'type': 'time', 'required': True,}),
+        }
