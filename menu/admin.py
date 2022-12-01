8d6216092ffd390c17364c4c91e80ee9107fb254
jalalk1244 <jalaluddin.boss@gmail.com>
2022-12-01 13:26:03 +0000

    Add list filters and search fields to the drinks and alergens admin list

--- menu/admin.py
+++ menu/admin.py
@@ -2,14 +2,27 @@ from django.contrib import admin
 from .models import Dish, Drink, Alergen
 
 
-admin.site.register(Drink)
-admin.site.register(Alergen)
-
-
 @admin.register(Dish)
 class DishAdmin(admin.ModelAdmin):
 
     list_filter = ('price', 'available')
-    list_display = ('name', 'price', 'calorie_amount', 'protien_amount', 'carbs_amount', 'fat_amount', 'available')
+    list_display = (
+        'name', 'price', 'calorie_amount',
+        'protien_amount', 'carbs_amount', 'fat_amount', 'available')
     search_fields = ['name', 'description']
     filter_horizontal = ('alergens',)
+
+
+@admin.register(Drink)
+class DrinkAdmin(admin.ModelAdmin):
+
+    list_filter = ('price', 'available')
+    list_display = ('name', 'price', 'available')
+    search_fields = ['name', 'description']
+
+
+@admin.register(Alergen)
+class Alergen(admin.ModelAdmin):
+
+    list_filter = ('name', 'description')
+    search_fields = ['name', 'description']
